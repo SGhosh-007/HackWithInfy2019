44fbb5d2b2037ba60079b569695d8335f7cf4322
SGhosh-007 <43071913+SGhosh-007@users.noreply.github.com>
2019-07-01 17:37:46 +0530

    Create Collision_Course.py

---
+++ Collision_Course.py
@@ -0,0 +1,23 @@
+s=list(range(0,speed_count))
+m=list(map(sum,zip(speed,s)))
+l=m[:pos]
+r=m[pos+1:]
+def leftchk(l,pos,m):
+    c=0
+    if min(l)>m[pos]:
+        c=pos
+    else:
+        while len(l)>0 and max(l)>m[pos]:
+            c+=1
+            l.remove(max(l))
+    return c
+def rightchk(r,pos,m):
+    c=0
+    if max(r)<m[pos]:
+        c=len(r)
+    else:
+        while len(r)>0 and min(r)<m[pos]:
+            c+=1
+            r.remove(max(r))
+    return c
+print(leftchk(l,pos,m)+rightchk(r,pos,m))
